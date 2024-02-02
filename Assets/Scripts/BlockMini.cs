ef980237c4a5bfd13a00702b7752ae7fdb7e1ddd
HieuLajj <hieulv@supga.net>
2024-02-02 17:03:03 +0700

    fff

---
+++ Assets/Scripts/BlockMini.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+
+public enum DirectionBlock
+{
+    Down,
+    Up,
+    Back,
+    Forward,
+    Left,
+    Right
+}
+public enum StatusBlock
+{
+    Die,
+    Normal,
+}
+
+public class BlockMini : MonoBehaviour
+{
+    public StatusBlock statusBlock = StatusBlock.Normal;
+
+
+    private Vector3 dir2;
+    private Vector3 dir;
+    private RaycastHit hit;
+    public DirectionBlock Direction;
+    public GameObject ModelBlock;
+    public bool checkTrigger = false;
+    private int intDirection;
+    private void OnEnable()
+    {
+        GetDirectionBlock(UnityEngine.Random.Range(1,6));
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        { 
+            checkTrigger = true;
+        }        
+    }
+
+    public void GetDirectionBlock(int input)
+    {
+        intDirection = input;
+        switch (input)
+        {
+            case 1:
+                Direction = DirectionBlock.Down;
+                dir2 = Vector3.down;
+                ModelBlock.transform.localEulerAngles = new Vector3(90, 0, 0);
+                break;
+            case 2:
+                Direction = DirectionBlock.Up;
+                dir2 = Vector3.up;
+                ModelBlock.transform.localEulerAngles = new Vector3(-90, 0, 0);
+                break;
+            case 3:
+                Direction = DirectionBlock.Back;
+                dir2 = Vector3.back;
+                ModelBlock.transform.localEulerAngles = new Vector3(180, 0, 0);
+                break;
+            case 4:
+                Direction = DirectionBlock.Forward;
+                ModelBlock.transform.localEulerAngles = new Vector3(0, 0, 0);
+                dir2 = Vector3.forward;
+                break;
+            case 5:
+                Direction = DirectionBlock.Left;
+                dir2 = Vector3.left;
+                ModelBlock.transform.localEulerAngles = new Vector3(0, -90, 0);
+                break;
+            case 6:
+                Direction = DirectionBlock.Right;
+                dir2 = Vector3.right;
+                ModelBlock.transform.localEulerAngles = new Vector3(0, 90, 0);
+                break;
+            default:
+                Direction = DirectionBlock.Right;
+                dir2 = Vector3.right;
+                ModelBlock.transform.localEulerAngles = new Vector3(0, 90, 0);
+                break;
+        }
+    }
+
+    public void checkRay()
+    {
+        checkDirection();
+        if (Physics.Raycast(transform.position, dir, out hit, Mathf.Infinity, (1 << 6)))
+        {
+            BlockMini blockcheck = hit.collider.GetComponent<BlockMini>();
+            if (blockcheck.statusBlock == StatusBlock.Die)
+            {
+                RunBlock();
+                return;
+            }
+        }
+        else
+        {
+            RunBlock();
+        }
+      //  checkRay2();
+    }
+
+    public void checkRay2()
+    {
+        checkDirection();
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, dir, Mathf.Infinity, (1 << 6));
+     
+        foreach (RaycastHit hit in hits)
+        {
+            BlockMini blockcheck = hit.collider.GetComponent<BlockMini>();
+            if(CheckBugOpposite(blockcheck)==true)
+            {
+                blockcheck.RandomIngore(intDirection);
+              //  blockcheck.gameObject.SetActive(false);
+            }
+          
+        }
+    }
+
+    public void checkDirection()
+    {
+        switch (Direction)
+        {
+            case DirectionBlock.Left:
+                dir = -transform.right;
+                break;
+            case DirectionBlock.Right:
+                dir = transform.right;
+                break;
+            case DirectionBlock.Up:
+                dir = transform.up;
+                break;
+            case DirectionBlock.Down:
+                dir = -transform.up;
+                break;
+            case DirectionBlock.Forward:
+                dir = transform.forward;
+                break;
+            case DirectionBlock.Back:
+                dir = -transform.forward;
+                break;
+            default:
+                dir = -transform.forward;
+                break;
+        }
+    }
+    public void RunBlock()
+    {    
+
+        statusBlock = StatusBlock.Die;
+        Vector3 dir3 = transform.TransformDirection(dir2);      
+        StartCoroutine(Run(dir3));
+    }
+    IEnumerator Run(Vector3 direction)
+    {
+       
+        float alpha = 12;
+        while (alpha >= 0)
+        {
+            transform.Translate(direction * 20 * Time.deltaTime, Space.World);
+            yield return null;
+            alpha -= 0.1f;
+        }
+        gameObject.SetActive(false);
+    }
+
+    public BlockMini CheckFast()
+    {
+        checkDirection();
+        if(statusBlock == StatusBlock.Die)
+        {
+            return null;
+        }
+
+        if (Physics.Raycast(transform.position, dir, out hit, Mathf.Infinity, 1 << 6))
+        {
+
+            BlockMini blockcheck = hit.collider.GetComponent<BlockMini>();
+
+            if (blockcheck.statusBlock == StatusBlock.Die)
+            {
+                return this;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+        {
+            return this;
+        }
+    }
+    public bool CheckBugOpposite(BlockMini blockOpposite)
+    {
+        bool checkboolbug = false;
+        switch (Direction)
+        {
+            case DirectionBlock.Down:
+                if (blockOpposite.Direction == DirectionBlock.Up)
+                {
+                    checkboolbug = true;
+                }
+                break;
+            case DirectionBlock.Up:
+                if (blockOpposite.Direction == DirectionBlock.Down)
+                {
+                    checkboolbug = true;
+                }
+                break;
+
+            case DirectionBlock.Forward:
+                if (blockOpposite.Direction == DirectionBlock.Back)
+                {
+                    checkboolbug = true;
+                }
+                break;
+            case DirectionBlock.Back:
+                if (blockOpposite.Direction == DirectionBlock.Forward)
+                {
+                    checkboolbug = true;
+                }
+                break;
+            case DirectionBlock.Left:
+                if (blockOpposite.Direction == DirectionBlock.Right)
+                {
+                    checkboolbug = true;
+                }
+                break;
+            case DirectionBlock.Right:
+                if (blockOpposite.Direction == DirectionBlock.Left)
+                {
+                    checkboolbug = true;
+                }
+                break;
+        }
+        return checkboolbug;
+    }
+
+    public void RandomIngore(int a)
+    {
+      
+        int randomNumber;
+
+        do
+        {
+            randomNumber = UnityEngine.Random.Range(1, 6); // Sinh số ngẫu nhiên từ 1 đến 6
+        } while (randomNumber == a); // Lặp lại nếu số ngẫu nhiên bằng 2
+
+        GetDirectionBlock(randomNumber);
+    }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using System.Xml;
+using System;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject blockDoidien;
+    private static Controller instance;
+    public static Controller Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<Controller>();
+            }
+            return instance;
+        }
+        private set
+        {
+            instance = value;
+        }
+    }
+
+    public Transform pretransform;
+
+    public List<BlockMini> blockMiniList;
+    public List<BlockMini> blockMiniListBansao;
+
+    public Dictionary<string, BlockMini> blockMiniDictionary = new Dictionary<string, BlockMini>();
+
+
+    public List<BlockMini> blockMiniListTemp_FilterObject;
+
+
+
+    public List<BlockMini> listcansapxeplai;
+
+    [Button()]    
+    
+    public void FilterObject()
+    {
+        for(int i = 0; i < blockMiniList.Count; i++)
+        {
+            if (blockMiniList[i].checkTrigger == false)
+            {
+                blockMiniList[i].gameObject.SetActive(false);
+                blockMiniListTemp_FilterObject.Add(blockMiniList[i]);
+            }
+            else
+            {
+
+                int xx = (int)(blockMiniList[i].transform.position.x * 10000);
+                int yy = (int)(blockMiniList[i].transform.position.y * 10000);
+                int zz = (int)(blockMiniList[i].transform.position.z * 10000);
+                string key = xx + "|" + yy + "|" + zz;
+                blockMiniDictionary.Add(key, blockMiniList[i]);
+            }
+        }
+
+
+        for(int i=0; i<blockMiniListTemp_FilterObject.Count; i++)
+        {
+            blockMiniList.Remove(blockMiniListTemp_FilterObject[i]);
+        }
+    }
+
+    // chuc nang doi xung
+
+    //đối xứng trục y qua ox (la thay doi z);
+    [Button()]
+    public void PrintArrayOy()
+    {
+        Debug.Log("dang tien hanh");
+        foreach (KeyValuePair<string, BlockMini> pair in blockMiniDictionary)
+        {
+           
+            string keyValue = pair.Key;
+            //Debug.Log(keyValue);
+            string[] numbersAsStrings = keyValue.Split('|');
+         
+            float[] numbersAsFloats = new float[numbersAsStrings.Length];
+       
+            for (int i = 0; i < numbersAsStrings.Length; i++)
+            {
+                numbersAsFloats[i] = float.Parse(numbersAsStrings[i]);
+              
+            }
+            string keyValueOpposite = numbersAsFloats[0] + "|" + numbersAsFloats[1] + "|" + -1 * numbersAsFloats[2];
+            if (blockMiniDictionary.ContainsKey(keyValueOpposite))
+            {
+            }
+            else
+            {
+                BlockMini blockMini = pair.Value;
+                blockMini.gameObject.SetActive(false);
+                blockMiniList.Remove(pair.Value);
+            }
+        }
+    }
+
+
+    //đối xứng trục y qua oz (la thay doi x);
+    [Button()]
+    public void PrintArray2Oy()
+    {
+        Debug.Log("dang tien hanh 2");
+
+        foreach (KeyValuePair<string, BlockMini> pair in blockMiniDictionary)
+        {
+            string keyValue = pair.Key;
+            string[] numbersAsStrings = keyValue.Split('|');
+            float[] numbersAsFloats = new float[numbersAsStrings.Length];
+
+            for (int i = 0; i < numbersAsStrings.Length; i++)
+            {
+                numbersAsFloats[i] = float.Parse(numbersAsStrings[i]);
+
+            }
+            string keyValueOpposite = -1 * numbersAsFloats[0] + "|" + numbersAsFloats[1] + "|" + numbersAsFloats[2];
+            if (blockMiniDictionary.ContainsKey(keyValueOpposite))
+            {
+            }
+            else
+            {
+                BlockMini blockMini = pair.Value;
+                blockMini.gameObject.SetActive(false);
+                blockMiniList.Remove(pair.Value);
+            }
+        }
+    }
+
+    [Button()]
+
+    public void TienhanhCheckNhanh()
+    {
+        blockMiniListBansao = blockMiniList;
+        CheckNhanh();
+    }
+    public List<BlockMini> ListenerBlock222 = new List<BlockMini>();
+
+    public void CheckNhanh()
+    {
+        int soluongtruocdo = blockMiniList.Count;
+        int soluongsaudo = soluongtruocdo;
+        foreach (var item in blockMiniList)
+        {
+            BlockMini blockmini = item.CheckFast();
+            if (blockmini != null)
+            {
+                blockmini.RunBlock();
+                soluongsaudo--;
+                if (listcansapxeplai.Contains(blockmini))
+                {
+                    listcansapxeplai.Remove(blockmini);
+                }
+                ListenerBlock222.Add(blockmini);
+            }
+            else
+            {
+                listcansapxeplai.Add(item);
+            }
+        }
+
+        foreach (var item in ListenerBlock222)
+        {
+            blockMiniList.Remove(item);
+        }
+        ListenerBlock222.Clear();
+
+        if (soluongsaudo == soluongtruocdo)
+        {
+            Debug.Log("KHong co duong thoat o level, can thuc hien lai vong lap");
+            for(int g=0; g < blockMiniList.Count; g++)
+            {
+               blockMiniList[g].GetDirectionBlock(UnityEngine.Random.Range(1, 6));
+            }
+            CheckNhanh();
+            return;
+        }
+        if (blockMiniList.Count == 0)
+        {
+            Debug.Log("hoan tat" +"===movethucte+++==============================");      
+            return;
+        }
+        CheckNhanh();
+    }
+
+
+    [Button]
+    public void TienhanhCheckNhanhCacKhoiDoiDau()
+    {
+        foreach (var item in blockMiniList)
+        {
+            item.checkRay2();
+        }
+
+    }
+
+
+}
